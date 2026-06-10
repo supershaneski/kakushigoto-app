9fe2a10b6aa1867b05a14c6e1211b4ae8ab5dac7
shaneski <shane@micronetclub.co.jp>
2026-06-10 14:33:42 +0900

    remove base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/kakushigoto-app/',
   build: {
     rollupOptions: {
       output: {
